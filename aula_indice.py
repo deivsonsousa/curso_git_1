c74840a11dfe88d4a09543924421ca7c10d8476b
deivssousa <jhonnyramone8@hotmail.com>
2022-06-22 20:10:38 -0300

    troca de nome e aula nova

---
+++ aula_indice.py
@@ -0,0 +1,11 @@
+"""
+cores = ['verde', 'amarelo', 'azul', 'branco']
+
+for indice, cor in enumerate(cores):
+    print(indice, cor)
+
+"""
+
+numeros = [5, 6, 7, 8, 9, 41, 45]
+print(numeros.index(8))
+
