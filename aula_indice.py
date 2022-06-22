bdcf251a9fd6b7565a0af835cece5d4b531c95e8
deivssousa <jhonnyramone8@hotmail.com>
2022-06-22 20:47:05 -0300

    troca de nome e aula nova.1

--- aula_indice.py
+++ aula_indice.py
@@ -4,11 +4,28 @@ cores = ['verde', 'amarelo', 'azul', 'branco']
 for indice, cor in enumerate(cores):
     print(indice, cor)
 
-"""
-
 numeros = [5, 6, 7, 8, 9, 41, 45]
 print(numeros.index(8))
 
 lista = [1, 2, 3, 4, 5,]
 print(lista[::2])
+
+"""
+
+#Deep Copy
+
+lista = [1, 2, 3]
+print(lista)
+
+nova = lista.copy()
+
+print(nova)
+
+nova.append(4)
+
+print(lista)
+print(nova)
+
+
+
 
