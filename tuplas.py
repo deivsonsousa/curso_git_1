14be8b19587a4d646d985f96c2fdd14cbc36d88f
deivssousa <jhonnyramone8@hotmail.com>
2022-06-24 19:06:14 -0300

    tuplas

---
+++ tuplas.py
@@ -0,0 +1,15 @@
+tupla1 = (1, 2, 3, 4, 5, 6)
+print(tupla1)
+
+print(type(tupla1))
+
+tupla2 = 1, 2, 3, 4, 5, 6, 7
+print(tupla2)
+
+print(type(tupla2))
+
+tupla3 = (1)
+
+print(tupla3)
+
+print(type(tupla3))
